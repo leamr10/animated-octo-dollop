8da9d6a8f6389674665adedf918e348ffad65560
leamr10 <leamr10@gmail.com>
2023-07-11 16:59:58 -0400

    post routes

--- routes/ApiRoutes.js
+++ routes/ApiRoutes.js
@@ -1,12 +1,18 @@
-const router = require ('express').Router()
+const router = require('express').Router()
+const UUID = require('../helpers/uuid')
+const { readFromFile, readAndAppend } = require("../helpers/fsUtils");
 
-const {readFromFile, readAndAppend} = require ("../helpers/fsUtils");
-
-router.get ("/notes", (req, res) => {
-    readFromFile("./db/db.json").then(data => res.json(JSON.parse(data)) )
+router.get("/notes", (req, res) => {
+    readFromFile("./db/db.json").then(data => res.json(JSON.parse(data)))
 })
 
-router.post (/"notes", (req, res) => {
-    readAndAppend
+router.post("/notes", (req, res) => {
+    if (req.body.title && req.body.text) {
+        let newNote = { title: req.body.title, text: req.body.text, id: UUID() };
+        readAndAppend(newNote, "./db/db.json")
+        res.json("Success!")
+    }
+    else res.json("Error!")
+
 }
-  )
+)
