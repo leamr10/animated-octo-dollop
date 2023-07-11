496c65d6f39e8366454d7194d6102c303157bcf0
leamr10 <leamr10@gmail.com>
2023-07-11 16:51:25 -0400

    get route

--- routes/ApiRoutes.js
+++ routes/ApiRoutes.js
@@ -1,25 +1,12 @@
-const express = require('express');
-const path = require('path');
-const api = require('./routes/ApiRoutes.js');
+const router = require ('express').Router()
 
-const PORT = process.env.PORT || 3001;
+const {readFromFile, readAndAppend} = require ("../helpers/fsUtils");
 
-const app = express();
+router.get ("/notes", (req, res) => {
+    readFromFile("./db/db.json").then(data => res.json(JSON.parse(data)) )
+})
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use('/api', api);
-
-app.use(express.static('public'));
-
-app.get('/', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/index.html'))
-);
-
-app.get('/notes', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/notes.html'))
-);
-
-app.listen(PORT, () =>
-  console.log(`App listening at http://localhost:${PORT} 🚀`)
-);
+router.post (/"notes", (req, res) => {
+    readAndAppend
+}
+  )
