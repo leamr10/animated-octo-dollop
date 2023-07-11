4da7f47e87c4441a2d51a86c451587e582efb27e
leamr10 <leamr10@gmail.com>
2023-07-11 17:24:22 -0400

    delete route

--- routes/ApiRoutes.js
+++ routes/ApiRoutes.js
@@ -1,6 +1,6 @@
 const router = require('express').Router()
 const UUID = require('../helpers/uuid')
-const { readFromFile, readAndAppend } = require("../helpers/fsUtils");
+const { readFromFile, readAndAppend, readAndRemove } = require("../helpers/fsUtils");
 
 router.get("/notes", (req, res) => {
     readFromFile("./db/db.json").then(data => res.json(JSON.parse(data)))
@@ -14,5 +14,11 @@ router.post("/notes", (req, res) => {
     }
     else res.json("Error!")
 
-}
-)
+})
+
+router.delete("/notes/:id", (req, res) => {
+    readAndRemove(req.params.id, "./db/db.json")
+    res.json("Success!")
+})
+
+module.exports = router;
